a5beca7804fc4d53de1dc111a21beb8a8b83e541
Michael Veilleux <>
2023-01-28 15:27:47 +0800

    update for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
