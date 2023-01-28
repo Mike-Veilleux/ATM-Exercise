f06387da32ed9c92a8ffdda3316e84fdad9d1f74
Michael Veilleux <>
2023-01-28 15:33:05 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
+    {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
+    <App />
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
